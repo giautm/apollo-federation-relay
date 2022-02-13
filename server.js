405d6d9e1b709219dccf899330ce963d1f613cc5
Giau. Tran Minh <giau.tmg@gmail.com>
2022-02-14 01:20:01 +0700

    fix: support PULID prefix

--- server.js
+++ server.js
@@ -4,30 +4,21 @@
 const { ApolloServer } = require('apollo-server');
 
 const { NodeGateway, NodeCompose } = require('./node-gateway');
-const { server: serverProduct } = require('./server-product');
-const { server: serverReview } = require('./server-review');
 
 const BASE_PORT = 8000;
 
-const SERVERS = [
-  { name: '📦 product', server: serverProduct },
-  { name: '🆒 review', server: serverReview },
-];
-
-async function startServers() {
-  const res = SERVERS.map(async ({ server, name }, index) => {
-    const number = index + 1;
-    const { url } = await server.listen(BASE_PORT + number);
-
-    console.log(`${name} up at ${url}graphql`);
-    return { name, url };
-  });
-
-  return await Promise.all(res);
-}
-
 async function main() {
-  const subgraphs = await startServers();
+  const subgraphs = [
+    {
+      name: 'Document',
+      url: 'http://localhost:5701/query',
+    },
+    {
+      name: 'Todo',
+      url: 'http://localhost:8701/query',
+    },
+  ]
+
   const gateway = new NodeGateway({
     supergraphSdl: new NodeCompose({ subgraphs }),
     serviceHealthCheck: true,
